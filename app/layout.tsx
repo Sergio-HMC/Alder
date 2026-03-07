1aecbf2d94cb602eb4f9be3e2cd49ea0279f6c4c
Sergio-HMC <contacto@homemarketconsulting.com>
2026-03-07 18:39:18 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Alder Metrics",
+  description: "Turning data into clear decisions",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
